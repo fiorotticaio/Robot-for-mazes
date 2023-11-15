3823381655b5d4850f4fdce838f1601cdef5a767
Vinicius Cole de Amorim <viniamorim87@hotmail.com>
2023-11-15 13:21:22 -0300

    aa

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
 /*======================== IMPORTAÇÕES ========================*/
 #include <LiquidCrystal_I2C.h>
+#include <Pololu3piPlus32U4.h>
+#include <PololuMenu.h>
 
+using namespace Pololu3piPlus32U4;
 
 /*======================== CONSTANTES ========================*/
 #define PINO_TRIGGER_SU 7 // Trigger sensor ultrassom
 #define PINO_ECHO_SU 8 // Echo sensor ultrassom
-
+#define NUM_SENSORS 5 // Número de sensores do robô (mudar se for 3)
 
 /*======================== PROGRAMA PRINCIPAL ========================*/
 /*===== Variáveis globais =====*/
 int colunas_lcd = 16; // Número de colunas do display LCD
 int linhas_lcd = 2; // Número de linhas do display LCD
 int endereco_lcd = 0x27; // Endereço do display LCD
+unsigned int valores_sensores[NUM_SENSORS];
 LiquidCrystal_I2C lcd(0x27, lcd_colunas, lcd_linhas); // Inicializando o display LCD
+Buzzer buzzer;
+LineSensors sensores_linha;
+Motors motores;
+ButtonA botao_a;
+ButtonB botao_b;
+ButtonC botao_c;
+int16_t ultimo_erro = 0;
+uint16_t velocidade_max = 200;                   // Velocidade máxima dos motores
+int16_t velocidade_min  = 0;                     // Velocidade mínima dos motores
+uint16_t velocidade_base = velocidade_max;       // Velocidade base dos motores quando o robô está centralizado na linha. Deixar em 0 para testar sem velocidade base.
+uint16_t velocidade_calibracao = 60;             // Velocidade dos motores durante a calibração.
+uint16_t coef_proporcional = 64;                 // Coeficiente do termo proporcional
+uint16_t coef_derivativo = 256;                  // Coeficiente do termo derivativo
 
 void setup() {
   Serial.begin(9600); // Inicializando a comunicação serial
@@ -22,6 +39,9 @@ void setup() {
 
   lcd.init(); // Inicializando o display LCD
   lcd.backlight(); // Acendendo o backlight do display LCD
+
+  while(!botao_b.getSingleDebouncedPress()); // Esperando o botão B ser pressionado para começar a calibração
+  calibra_sensores(); 
 }
 
 void loop() {
@@ -39,7 +59,22 @@ void loop() {
   /* Caminho do robo livre, segue reto */
 
 
+  //código do 3pi
 
+  int16_t posicao = sensores_linha.readLineBlack(valores_sensores);
+  int16_t erro = posicao - 2000;
+
+  int16_t diff_velocidade = erro * (int32_t)coef_proporcional / 256  + (erro - ultimo_erro) * (int32_t)coef_derivativo / 256;
+
+  ultimo_erro = erro;
+
+  int16_t velocidade_esq = (int16_t)velocidade_base + diff_velocidade;
+  int16_t velocidade_dir = (int16_t)velocidade_base - diff_velocidade;
+
+  velocidade_esq = constrain(velocidade_esq, velocidade_min, (int16_t)velocidade_max);
+  velocidade_dir = constrain(velocidade_dir, velocidade_min, (int16_t)velocidade_max);
+
+  motors.setSpeeds(velocidade_esq, velocidade_dir);
 }
 
 
@@ -62,4 +97,28 @@ long mede_distancia_su(int pino_trigger, int pino_echo) {
 
 long microssegundos_para_cm(long microssegundos) {
   return microssegundos / 29 / 2;
+}
+
+void calibra_sensores()
+{
+  //todo implementar display
+  //display.clear();
+
+  // espera 1 segundo e começa a calibração
+  // rodando e passando os sensores por cima da linha
+  delay(1000);
+  for(uint16_t i = 0; i < 80; i++)
+  {
+    if (i > 20 && i <= 60)
+    {
+      motores.setSpeeds(-(int16_t)velocidade_calibracao, velocidade_calibracao);
+    }
+    else
+    {
+      motors.setSpeeds(velocidade_calibracao, -(int16_t)velocidade_calibracao);
+    }
+
+    sensores_linha.calibrate();
+  }
+  motores.setSpeeds(0, 0);
 }
