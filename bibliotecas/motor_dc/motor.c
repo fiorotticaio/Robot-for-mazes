50060713f86d573fb62407f7de64211fc127506c
Matheus <mt.schreiber01@gmail.com>
2023-12-02 18:39:18 -0300

    :recycle: modularizando codigo dos motores dc

--- bibliotecas/motor_dc/motor.c
+++ bibliotecas/motor_dc/motor.c
@@ -1,29 +1,58 @@
 #include "bibliotecas/motor_dc/motor.h"
 
-void setup() {
+void init_motores_dc() {
+  // Configurando pinos de enable dos motores 
   pinMode(PINO_E_M1, OUTPUT);
   pinMode(PINO_E_M2, OUTPUT);
+
+  // Configurando pinos do PWM dos motores
   pinMode(PINO_M1, OUTPUT);
   pinMode(PINO_M2, OUTPUT);
 
+  // Sincronizando um pino a um canal
   ledcAttachPin(PINO_M1, PWM1_CH);
   ledcSetup(PWM1_CH, PWM1_FREQ, PWM1_RES);
 
+  // Sincronizando um pino a um canal
   ledcAttachPin(PINO_M2, PWM2_CH);
   ledcSetup(PWM2_CH, PWM1_FREQ, PWM1_RES);
 }
 
-
-void loop() {
+// Função que liga os motores (sinal de enable HIGH)
+void liga_motores(){
   digitalWrite(PINO_E_M1, HIGH);
   digitalWrite(PINO_E_M2, HIGH);
+}
 
-  ledcWrite(PWM1_CH, 50);
-  ledcWrite(PWM2_Ch, 200);
+// Função que liga os motores (sinal de enable LOW)
+void desliga_motores(){
+  digitalWrite(PINO_E_M1, LOW);
+  digitalWrite(PINO_E_M2, LOW);
+}
 
-  delay(1000);
-
+// Função para andar pra frente sem parar
+void anda_pra_frente(){
   ledcWrite(PWM1_CH, 200);
-  ledcWrite(PWM2_Ch, 50);
-  delay(1000);
+  ledcWrite(PWM2_CH, 200);
+}
+
+// Função que freia, lendo sensores para saber se já pode parar de desacelerar
+void freia_ate_parar() {
+  // TODO: precisa adicionar dinamica com sensores
+  ledcWrite(PWM1_CH, 50);
+  ledcWrite(PWM2_CH, 50);
+}
+
+// Função que vira o carrinho para esquerda, usando sensores
+void vira_pra_esquerda(){
+  // TODO: precisa adicionar dinamica com sensores
+  ledcWrite(PWM1_CH, 50);
+  ledcWrite(PWM2_CH, 200);
+}
+
+// Função que vira  o carrinho para direita, usando sensores
+void vira_pra_direita(){
+  // TODO: precisa adicionar dinamica com sensores
+  ledcWrite(PWM1_CH, 50);
+  ledcWrite(PWM2_CH, 200);
 }
--- bibliotecas/motor_dc/motor.h
+++ bibliotecas/motor_dc/motor.h
@@ -1,7 +1,13 @@
-#define PWMIN1 ?
-#define PWMIN2 ?
+#ifndef MOTORES_DC_H
+#define MOTORES_DC_H
 
-#define PWMOUT1 ?
-#define PWMOUT2 ?
+#define PINO_E_M1 33
+#define PINO_E_M2 26
+#define PINO_M1 25
+#define PINO_M2 27
+#define PWM1_CH 0
+#define PWM2_CH 1
+#define PWM1_FREQ 1000  // 1 kHz
+#define PWM1_RES 8      // 8 bits
 
-void set_motors(int m1, int m2);
+#endif
