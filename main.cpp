c7a0032b27a238538b93b34b5dcccc6fb2f7d818
Caio Alves Fiorotti <caiofiorotti@gmail.com>
2023-12-04 19:58:51 -0300

    :construction: adicionando códigos infra e motor pra testar

--- main.cpp
+++ main.cpp
@@ -1,183 +1,293 @@
-/*======================== IMPORTAÇÕES ========================*/
-#include "bibliotecas/lcd/lcd.h"
-#include "bibliotecas/sensor_ultrasson/sensor_ultrasson.h"
-#include "bibliotecas/motor/motor.h"
-#include "bibliotecas/servo/servo.h"
-#include "bibliotecas/sensor_infra/sensor_infra.h"
+/*======================== Sensor infravermelho (SI) ========================*/
+/*
+Valor do pino lendo total branco: 0
+Valor do pino lendo total preto: 3800
+*/
+
+#define SI_PINO_SL 6
+#define SI_PINO_SC 5
+#define SI_PINO_SR 4
+#define SI_PINO_SLC 3
+#define SI_PINO_SRC 7
+
+void SI_init_sensor_infra();
 
 
+/*======================== Motor DC (MDC) ========================*/
+#define MDC_PINO_E_M1 33
+#define MDC_PINO_E_M2 26
+#define MDC_PINO_M1 25
+#define MDC_PINO_M2 27
+#define MDC_PWM1_CH 0 //FIXME: Esquerda ou direita? 
+#define MDC_PWM2_CH 1 //FIXME: Esquerda ou direita? vamos descobrir...
+#define MDC_PWM1_FREQ 1000  // 1 kHz
+#define MDC_PWM1_RES 8      // 8 bits
+#define DIREITA 1
+#define ESQUERDA 0
+#define TRAS -1
 
-/*======================== CONSTANTES ========================*/
+void MDC_init_motores_dc() ;
+void MDC_liga_motores();
+void MDC_desliga_motores();
+void MDC_anda_pra_frente();
+void MDC_freia_ate_parar();
+void MDC_vira(char direcao);
+int MDC_vira_pra_esquerda(int sensorInfraLeft, int sensorInfraCenter, int achouLinha);
+int MDC_vira_pra_direita(int sensorInfraRight, int sensorInfraCenter, int achouLinha);
+int MDC_vira_180_graus(int sensorInfraLeft, int sensorInfraRight, int sensorInfraCenter, int achouLinha);
+
+int MDC_esta_virando = 0;         // 1 está virando, 0 não está virando
+int MDC_direcao_esta_virando = DIREITA;
+int MDC_achou_linha_virando = 0;  //  0: ainda nao achou a linha
+                                  //  1: achou a linha, está quase terminando de virar
+                                  // -1: terminou de virar
+float MDC_kp = 0.0334210526;      // constante proporcional
+/* 127 / 3800 = 0.0334210526 */
 
 
-
-/*======================== Variáveis ========================*/
-long distancia_parede = 0; // Distância anterior medida pelo sensor ultrassom
-char distancia_parede_lcd[16];
-int esta_virando = 0;         // 1 está virando, 0 não está virando
-int direcao_esta_virando = 1; // 1 direita, 0 esquerda, -1 trás
-
-int achou_linha_virando = 0;  //  0: ainda nao achou a linha
-                              //  1: achou a linha, está quase terminando de virar
-                              // -1: terminou de virar
-
-float kp = 0.1;               // constante proporcional
-                              //FIXME: achar um valor bom para kp e kd
-float kd = 0.05;              // constante derivativa
-
-float ultimo_erro = 0;        // erro anterior (apenas para termo derivativo)
-float erro = 0;               // erro atual
 
 /*======================== PROGRAMA PRINCIPAL ========================*/
 void setup() {
   Serial.begin(9600); // Inicializando a comunicação serial
 
-  /* Inicialização dos pinos */
-  pinMode(PINO_TRIGGER_SU, OUTPUT);
-  pinMode(PINO_ECHO_SU, INPUT);
-  pinMode(RS, OUTPUT);
-  pinMode(EN, OUTPUT);
-  for(int i = 0; i < 4; i++) {
-    pinMode(DATA[i], OUTPUT);
-  }
-  pinMode(PINO_SL, INPUT);
-  pinMode(PINO_SC, INPUT);
-  pinMode(PINO_SR, INPUT);
-  pinMode(PINO_SLC, INPUT);
-  pinMode(PINO_SRC, INPUT);
-
-  /* Inicialização do LCD */
-  lcd_init();
-  write8bits(CLEAR_DISPLAY);
-  write8bits(RETURN_HOME);
-  delay(2); // 1.52 ms de delay necessários para o comando de Return Home
-  writeData("Distancia us: ");
+  SI_init_sensor_infra(); // Inicializando os sensores infra vermelho
+  MDC_init_motores_dc(); // Inicializando os motores DC
 }
 
 void loop() {
-  long distancia_parede = mede_distancia_su(PINO_TRIGGER_SU, PINO_ECHO_SU, distancia_parede);
-
-  // Escrevendo a distância do ultrassom no display
-  sprintf(distancia_parede_lcd, "%d", distancia_parede);
-  writeData(distancia_parede_lcd);
-
-  /* Dependendo da quantidade de casas decimais, tem que voltar mais o cursor */
-  if (i < 10) {
-    write8bits(MOVE_CURSOR_LEFT);
-    delay(2);
-    
-  } else if (i < 100) {
-    write8bits(MOVE_CURSOR_LEFT);
-    delay(2);
-    write8bits(MOVE_CURSOR_LEFT);
-    delay(2);
-  
-  } else {
-    write8bits(MOVE_CURSOR_LEFT);
-    delay(2);
-    write8bits(MOVE_CURSOR_LEFT);
-    delay(2);
-    write8bits(MOVE_CURSOR_LEFT);
-    delay(2);
-  }
-
-
   /* Lendo os sensores infra vermelho */
-  int sl = analogRead(PINO_SL); 
-  int sc = analogRead(PINO_SC);
-  int sr = analogRead(PINO_SR);
-  int slc = analogRead(PINO_SLC);
-  int src = analogRead(PINO_SRC);
-  // Pino analógico de cada um deles? acho q sim
+  int sl = analogRead(SI_PINO_SL); 
+  int sc = analogRead(SI_PINO_SC);
+  int sr = analogRead(SI_PINO_SR);
+  int slc = analogRead(SI_PINO_SLC);
+  int src = analogRead(SI_PINO_SRC);
   
-  //FIXME: adicionar um threshold se pa
+  // FIXME: adicionar um threshold se pa
   // Maior que 0: lendo linha
   // Igual a 0: lendo branco
 
-  // caso não esteja virando, apenas siga a linha
-  if (!esta_virando) {
-
-    /* Verifica cruzamento */
-    if (sc > 0 && sl > 0 && sr > 0) {
-      // TODO: talvez aqui deve ter que adicionar um delay para o robo nao ficar virando varias vezes sem parar
-      esta_virando = 1;
-      if(slc > 0 && src > 0) {
-        // cruzamento em T
-        direcao_esta_virando = 0;   //sempre vira para esquerda
-      } else if (slc > 0) {
-        direcao_esta_virando = 0;
-      } else if (src > 0) {
-        direcao_esta_virando = 1;
+  /* Caso não esteja virando, apenas siga a linha */
+  if (!MDC_esta_virando) {
+    if (sc > 0 && sl > 0 && sr > 0) { // Verifica cruzamento
+      delay(200);
+      MDC_esta_virando = 1;
+      if(sl > 0 && sr > 0) { // Cruzamento em T
+        MDC_direcao_esta_virando = ESQUERDA; // Sempre vira para esquerda
+      } else if (sl > 0) {
+        MDC_direcao_esta_virando = ESQUERDA;
+      } else if (sr > 0) {
+        MDC_direcao_esta_virando = DIREITA;
       } else {
-        direcao_esta_virando = 0;   //sempre vira para esquerda
+        MDC_esta_virando = 0; // Não é um cruzamento
       }
     }
     
     /* Seguir linha */
     if (slc == 0 && src == 0) {
-      anda_pra_frente();
-    } else if (slc > 0 && src == 0 || slc == 0 && src > 0) {
-      // Virar a direita com controle proporcional
-      erro = medir_erro(slc, sc, src);
-      float controle = kp*erro + kd*(erro - ultimo_erro);
-      int velocidadeEsq = 127 + controle;
-      int velocidadeDir = 127 - controle;
+      MDC_anda_pra_frente();
+
+    } else if ((slc > 0 && src == 0) || (slc == 0 && src > 0)) {
+      /* Virar a direita com controle proporcional */
+      int velocidadeDir = 127 + MDC_kp * slc;
+      int velocidadeEsq = 127 + MDC_kp * src;
       
-      ultimo_erro = erro;
+      ledcWrite(MDC_PWM1_CH, velocidadeEsq);
+      ledcWrite(MDC_PWM2_CH, velocidadeDir);
 
-      ledcWrite(PWM1_CH, velocidadeEsq);
-      ledcWrite(PWM2_CH, velocidadeDir);
-
-    } else if (slc > 0 && src > 0) {
-      // ??????
-      Serial.println("cruzamento chegando");
-    } else if(sl == 0 && sc == 0 && sr == 0 && slc == 0 && src == 0) {
-      //caminho sem saida
-      esta_virando = 1;
-      direcao_esta_virando = -1;
+    } else if (sl == 0 && sc == 0 && sr == 0 && slc == 0 && src == 0) { // Caminho sem saida, fica parado
+      ledcWrite(MDC_PWM1_CH, 0);
+      ledcWrite(MDC_PWM2_CH, 0);
     }
-  // caso esteja virando, então continue virando até terminar
-  } else {
     
-    // virando para esquerda
-    if (direcao_esta_virando == 0) {
-      achou_linha_virando = vira_pra_esquerda(sl, sc, achou_linha_virando);
+  } else { // Caso esteja virando, então continue virando até terminar
+    
+    /* Virando para esquerda */
+    if (MDC_direcao_esta_virando == ESQUERDA) {
+      MDC_achou_linha_virando = MDC_vira_pra_esquerda(sl, sc, MDC_achou_linha_virando);
       
-      // terminou de virar
-      if (achou_linha_virando==-1){
-        esta_virando = 0;
+      /* Terminou de virar */
+      if (MDC_achou_linha_virando == -1){
+        MDC_esta_virando = 0;
       }
 
-    }
-    
-    // virando para a direita
-    else if (direcao_esta_virando == 1) {
-      achou_linha_virando = vira_pra_direita(sr, sc, achou_linha_virando);
+    } else if (MDC_direcao_esta_virando == 1) { // Virando para a direita
+      MDC_achou_linha_virando = MDC_vira_pra_direita(sr, sc, MDC_achou_linha_virando);
       
-      // terminou de virar
-      if (achou_linha_virando==-1){
-        esta_virando = 0;
+      /* Terminou de virar */
+      if (MDC_achou_linha_virando == -1){
+        MDC_esta_virando = 0;
       }
     }
 
-    // virando para trás
-    else if (direcao_esta_virando == -1) {
-      achou_linha_virando = vira_180_graus(sl, sr, sc, achou_linha_virando);
+    /* Virando para trás */
+    else if (MDC_direcao_esta_virando == -1) {
+      MDC_achou_linha_virando = MDC_vira_180_graus(sl, sr, sc, MDC_achou_linha_virando);
       
-      // terminou de virar
-      if (achou_linha_virando==-1){
-        esta_virando = 0;
+      /* Terminou de virar */
+      if (MDC_achou_linha_virando == -1) {
+        MDC_esta_virando = 0;
       }
     }
   }
-
-
-  // TODO:
-  delay(500); // Só para testes
 }
 
-/*======================== FUNÇÕES ========================*/
 
-float medir_erro(int slc, int sc, int src) {
-  return sc - (slc + src)/2;    
+/*======================== Sensor infravermelho (SI) ========================*/
+/* Função que inicializa os sensores infravermelhos */
+void SI_init_sensor_infra() {
+  pinMode(SI_PINO_SL, INPUT);
+  pinMode(SI_PINO_SC, INPUT);
+  pinMode(SI_PINO_SR, INPUT);
+  pinMode(SI_PINO_SLC, INPUT);
+  pinMode(SI_PINO_SRC, INPUT);
+}
+
+
+/*======================== Motor DC (MDC) ========================*/
+/* Função que inicializa os motores DC */
+void MDC_init_motores_dc() {
+  // Configurando pinos de enable dos motores 
+  pinMode(MDC_PINO_E_M1, OUTPUT);
+  pinMode(MDC_PINO_E_M2, OUTPUT);
+
+  // Configurando pinos do PWM dos motores
+  pinMode(MDC_PINO_M1, OUTPUT);
+  pinMode(MDC_PINO_M2, OUTPUT);
+
+  // Sincronizando um pino a um canal
+  ledcAttachPin(MDC_PINO_M1, MDC_PWM1_CH);
+  ledcSetup(MDC_PWM1_CH, MDC_PWM1_FREQ, MDC_PWM1_RES);
+
+  // Sincronizando um pino a um canal
+  ledcAttachPin(MDC_PINO_M2, MDC_PWM2_CH);
+  ledcSetup(MDC_PWM2_CH, MDC_PWM1_FREQ, MDC_PWM1_RES);
+}
+
+/* Função que liga os motores (sinal de enable HIGH) */
+void MDC_liga_motores(){
+  digitalWrite(MDC_PINO_E_M1, HIGH);
+  digitalWrite(MDC_PINO_E_M2, HIGH);
+}
+
+/* Função que liga os motores (sinal de enable LOW) */
+void MDC_desliga_motores(){
+  digitalWrite(MDC_PINO_E_M1, LOW);
+  digitalWrite(MDC_PINO_E_M2, LOW);
+}
+
+/* Função para andar pra frente sem parar */
+void MDC_anda_pra_frente(){
+  ledcWrite(MDC_PWM1_CH, 200);
+  ledcWrite(MDC_PWM2_CH, 200);
+}
+
+/* Função que freia, lendo sensores para saber se já pode parar de desacelerar */
+void MDC_freia_ate_parar() {
+  // TODO: precisa adicionar dinamica com sensores
+  ledcWrite(MDC_PWM1_CH, 50);
+  ledcWrite(MDC_PWM2_CH, 50);
+}
+
+/* Função que vira o robô para esquerda, direita ou 180 graus */
+void MDC_vira(char direcao){
+  // 0 - 127 = trás
+  // 128 - 255 = frente
+  switch(direcao) {
+    case 'L':
+        // Vira para esquerda
+        ledcWrite(MDC_PWM1_CH, 54);
+        ledcWrite(MDC_PWM2_CH, 200);
+        delay(200);
+        break;
+    case 'R':
+        // Virar para direita
+        ledcWrite(MDC_PWM1_CH, 200);
+        ledcWrite(MDC_PWM2_CH, 54);
+        delay(200);
+        break;
+    case 'B':
+        // Vira 180 graus
+        ledcWrite(MDC_PWM1_CH, 54);
+        ledcWrite(MDC_PWM2_CH, 200);
+        delay(400);
+        break;
+    case 'S':
+        // Faz nada
+        break;
+  }
+}
+
+/* Função que vira o robô para esquerda, usando sensores */
+int MDC_vira_pra_esquerda(int sensorInfraLeft, int sensorInfraCenter, int achouLinha){
+
+  // setando enable nos motores
+  MDC_liga_motores();
+  int achou_linha_update = achouLinha;
+
+  // Verificando se o sensor da esquerda achou a linha (significa que deve continuar a virar mas ta quase chegando)
+  if (sensorInfraLeft > 0) {
+    achou_linha_update = 1;  
+  }
+
+  // Verificando se o sensor do centro achou a linha (significa que deve parar de virar)
+  if (sensorInfraCenter > 0) {
+    achou_linha_update = -1;
+  }
+
+  // Continuar virando para esquerda
+  if (achou_linha_update > 0){
+    MDC_vira('L');
+  }
+
+  return achou_linha_update;
+}
+
+/* Função que vira  o robô para direita, usando sensores */
+int MDC_vira_pra_direita(int sensorInfraRight, int sensorInfraCenter, int achouLinha){
+  
+  // setando enable nos motores
+  MDC_liga_motores();
+  int achou_linha_update = achouLinha;
+
+  // Verificando se o sensor da direita achou a linha (significa que deve continuar a virar mas ta quase chegando)
+  if (sensorInfraRight > 0) {
+    achou_linha_update = 1;  
+  }
+
+  // Verificando se o sensor do centro achou a linha (significa que deve parar de virar)
+  if (sensorInfraCenter > 0) {
+    achou_linha_update = -1;
+  }
+
+  // Continuar virando para esquerda
+  if (achou_linha_update > 0) {
+    MDC_vira('R');
+  }
+
+  return achou_linha_update;
+}
+
+/* Função que vira o robô 180 graus, usando sensores */
+int MDC_vira_180_graus(int sensorInfraLeft, int sensorInfraRight, int sensorInfraCenter, int achouLinha){
+  
+  // setando enable nos motores
+  MDC_liga_motores();
+  int achou_linha_update = achouLinha;
+
+  // Verificando se o sensor da esquerda achou a linha (significa que deve continuar a virar mas ta quase chegando)
+  if (sensorInfraRight > 0 || sensorInfraLeft > 0) {
+    achou_linha_update = 1;  
+  }
+
+  // Verificando se o sensor do centro achou a linha (significa que deve parar de virar)
+  if (sensorInfraCenter > 0) {
+    achou_linha_update = -1;
+  }
+
+  // Continuar virando para esquerda
+  if (achou_linha_update > 0) {
+    MDC_vira('B');
+  }
+
+  return achou_linha_update;
 }
